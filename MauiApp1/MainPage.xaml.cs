31a4913efee759a5b846e491d3e52bf874109ef3
bricefandio <bricegeorgyfandio@yahoo.fr>
2023-09-24 08:13:18 +0200

    Ajoutez des fichiers projet.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using QRCoder;
+using System;
+using System.IO;
+using Microsoft.Maui.Controls;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnGenerateClicked(object sender, EventArgs e)
+        {
+            var data = $"{InputText.Text}-{count++}"; // Ajoute un suffixe unique aux données pour les rendre différentes à chaque génération
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.L);
+            PngByteQRCode qRCode = new PngByteQRCode(qrCodeData);
+            byte[] qrCodeBytes = qRCode.GetGraphic(20);
+            QrCodeImage.Source = ImageSource.FromStream(() => new MemoryStream(qrCodeBytes));
+        }
+    }
+}
